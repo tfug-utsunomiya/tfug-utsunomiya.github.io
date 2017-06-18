b52b759441f3cc4e444aadf63507c4d3059dc085
Akimasa KIMURA <kimura.akim.asa@gmail.com>
2017-06-18 16:41:52 +0900

    Add pelican.

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'tfug-utsunomiya'
+SITENAME = 'TensorFlow Utsunomiya Blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'ja'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('TensorFlow', 'https://www.tensorflow.org/'),
+         ('TensorFlow User Group Utsunomiya', 'https://tfug-utsunomiya.connpass.com/'),
+         ('Python.org', 'http://python.org/'),)
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/tfug-utsunomiya'),
+          ('Twitter', 'https://twitter.com/search?q=%23tfug_utsunomiya'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
